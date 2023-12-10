5b7606f6016acf7714546c47becf8d0879eb7e56
Seleem <sseleem1601@gmail.com>
2023-12-10 16:29:33 -0500

    Add containers to readingquestion

--- src/pages/ReadingQuestion.js
+++ src/pages/ReadingQuestion.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom'; // Import useParams
-import { Box, Container, VStack, Text, Divider } from '@chakra-ui/react';
+import { Box, Flex, Container, VStack, Text, Divider } from '@chakra-ui/react';
 import { app } from '../firebase';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import Navbar from '../components/Navbar';
 
 const ReadingQuestion = () => {
     const [passageText, setPassageText] = useState('');
+    const [passageTitle, setPassageTitle] = useState('');
     const [questionGroups, setQuestionGroups] = useState([]);
 
     // Use useParams to get the passageId from the URL
@@ -20,9 +21,9 @@ const ReadingQuestion = () => {
 
             if (docSnap.exists()) {
                 const data = docSnap.data();
-                console.log(data.passageText);
                 setPassageText(data.passageText);
                 setQuestionGroups(data.questionGroups); // Assuming questionGroups is part of your document
+                setPassageTitle(data.passageTitle);
             } else {
                 console.log("No such document!");
             }
@@ -33,30 +34,49 @@ const ReadingQuestion = () => {
 
     return (
         <>
-        <Navbar />
-        
-        <Container maxW="container.xl">
-            
-            <VStack spacing={8}>
-                <Box p={5} shadow="md" borderWidth="1px">
-                    <Text fontWeight="bold">Passage Text:</Text>
-                    <Divider my={4} />
-                    <Text>{passageText}</Text>
-                </Box>
+            <Navbar />
+            <Container maxW="container.xl">
+                <Flex 
+                    direction={{ base: "column", md: "row" }} 
+                    spacing={8} 
+                    align="stretch" 
+                    my={5} // Vertical margin
+                >
+                    <Box 
+                        flex="1" 
+                        p={5} 
+                        shadow="md" 
+                        borderWidth="1px" 
+                        overflowY="auto" 
+                        maxH= {{base: "40vh", md: "75vh"}}
+                        mb={{ base: 4, md: 0 }} // Margin bottom on mobile
+                        mx = {{md:2}}
+                    >
+                        <Text fontWeight="bold">{passageTitle}:</Text>
+                        <Divider my={4} />
+                        <Box dangerouslySetInnerHTML={{ __html: passageText }} />
+                    </Box>
 
-                <Box p={5} shadow="md" borderWidth="1px">
-                    <Text fontWeight="bold">Questions:</Text>
-                    <Divider my={4} />
-                    {questionGroups.map((group, index) => (
-                        <Box key={index}>
-                            {/* Render your questions here */}
-                            <Text>{group.questionText}</Text>
-                            {/* More question details */}
-                        </Box>
-                    ))}
-                </Box>
-            </VStack>
-        </Container>
+                    <Box 
+                        flex="1" 
+                        p={5} 
+                        shadow="md" 
+                        borderWidth="1px" 
+                        overflowY="auto" 
+                        maxH="50vh" // Adjusted height
+                        mx = {{md:2}}
+                    >
+                        <Text fontWeight="bold">Questions:</Text>
+                        <Divider my={4} />
+                        {questionGroups.map((group, index) => (
+                            <Box key={index}>
+                                <Text>{group.questionText}</Text>
+                                {/* More question details */}
+                            </Box>
+                        ))}
+                    </Box>
+                </Flex>
+            </Container>
         </>
     );
 };
