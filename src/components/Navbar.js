afed02fdb69b58a100bcbbd3e67beecc57c8c2d4
Seleem <sseleem1601@gmail.com>
2023-12-10 08:07:55 -0500

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,24 +9,30 @@ import {
 } from '@chakra-ui/react';
 
 const Navbar = () => {
+    const buttonStyle = {
+        fontWeight: "bold",
+        transition: "all 0.2s cubic-bezier(.08,.52,.52,1)",
+        bgGradient: "linear(to-r, teal.500, green.500)",
+        color: "white",
+        _hover: {
+            bgGradient: "linear(to-r, teal.600, green.600)",
+            boxShadow: "md"
+        }
+    };
+
     return (
-        <Flex as="header" bg="black" p={4} color="white" align="center">
+        <Flex as="header" bg="black" p={3} color="white" align="center"> {/* Reduced padding */}
             {/* Logo */}
-            <Box p="2">
-                <Image src="/image.png" alt="Logo" boxSize="50px" />
+            <Box p="1">
+                <Image src="/image.png" alt="Logo" boxSize="50px" /> {/* Adjusted box size */}
             </Box>
 
             <Spacer />
 
-            {/* Navigation Items */}
-            <HStack spacing={4}>
-                <Button colorScheme="teal" variant="solid" fontWeight="bold" _hover={{ bg: "teal.600" }}>
-                    Problems
-                </Button>
-                <Button colorScheme="teal" variant="solid" fontWeight="bold" _hover={{ bg: "teal.600" }}>
-                    Log In
-                </Button>
-            </HStack>
+            {/* Navigation Item */}
+            <Button {...buttonStyle} size="md">
+                Log In
+            </Button>
         </Flex>
     );
 }
