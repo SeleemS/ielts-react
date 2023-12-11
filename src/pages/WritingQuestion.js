f4dc8e1226cd4569fa18fcc94ca59695c8882eb6
Seleem <sseleem1601@gmail.com>
2023-12-11 12:05:13 -0500

    Scoring writing questions

--- src/pages/WritingQuestion.js
+++ src/pages/WritingQuestion.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { Box,Button, Textarea, Flex, Container, Text, Divider } from '@chakra-ui/react';
+import { Box, Button, Textarea, Flex, Container, Text, Divider, useToast } from '@chakra-ui/react';
 import { app } from '../firebase';
 import { getFirestore, doc, getDoc } from 'firebase/firestore';
 import Navbar from '../components/Navbar';
@@ -13,12 +13,15 @@ import {
     ModalBody,
     ModalCloseButton,
     useDisclosure,
-  } from '@chakra-ui/react';
+} from '@chakra-ui/react';
 
 const WritingQuestion = () => {
     const [passageText, setPassageText] = useState('');
     const [passageTitle, setPassageTitle] = useState('');
     const [userResponse, setUserResponse] = useState('');
+    const [apiResponse, setApiResponse] = useState('');
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const toast = useToast();
 
     // Use useParams to get the docId from the URL
     const { id: docId } = useParams();
@@ -54,6 +57,46 @@ const WritingQuestion = () => {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        const wordCount = countWords(userResponse);
+        
+        if (wordCount < 250) {
+            toast({
+                title: "Word Count Too Low",
+                description: `Your answer must be at least 250 words long. Your current word count is: ${wordCount}`,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+            });
+            return;
+        }
+
+        const apiUrl = "https://wamm2ytjk5.execute-api.us-east-1.amazonaws.com/IELTSWritingBot";
+
+        try {
+            const response = await fetch(apiUrl, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ question: passageText, answer: userResponse })
+            });
+
+            if (response.ok) {
+                const responseData = await response.json(); // Assuming the response is JSON formatted
+                setApiResponse(responseData.message);
+                onOpen(); // Open the modal with the response
+            } else {
+                toast({
+                    title: "Error",
+                    description: "Failed to score the answer. Please try again.",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
     };
 
     return (
@@ -114,7 +157,21 @@ const WritingQuestion = () => {
                         Submit
                     </Button>
                 </Flex>
-                {/* Add a submit button if needed */}
+                <Modal isOpen={isOpen} onClose={onClose} isCentered size="xs">
+                    <ModalOverlay />
+                    <ModalContent mx={4} my="auto" maxW="400px">
+                        <ModalHeader>Your Score</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody overflowY="auto" maxH="250px">
+                            <Text>{apiResponse}</Text>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme="blue" mr={3} onClick={onClose}>
+                                Close
+                            </Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
             </Container>
         </>
     );
