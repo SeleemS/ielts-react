b842dba090fbc0437f27dff06420c5fbb1feabfe
Seleem <sseleem1601@gmail.com>
2023-12-10 20:33:51 -0500

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,40 +3,57 @@ import {
   Flex,
   Box,
   Spacer,
-  Button,
   Image,
-  HStack,
+  Text,
+  IconButton,
+  useDisclosure,
+  Collapse,
   Link
 } from '@chakra-ui/react';
-import { Link as RouterLink } from 'react-router-dom'; // Import RouterLink from react-router-dom
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { Link as RouterLink } from 'react-router-dom';
 
 const Navbar = () => {
-    const buttonStyle = {
-        fontWeight: "bold",
-        transition: "all 0.2s cubic-bezier(.08,.52,.52,1)",
-        bgGradient: "linear(to-r, teal.500, green.500)",
-        color: "white",
-        _hover: {
-            bgGradient: "linear(to-r, teal.600, green.600)",
-            boxShadow: "md"
-        }
-    };
+    const { isOpen, onToggle } = useDisclosure();
 
     return (
-        <Flex as="header" bg="black" p={3} color="white" align="center"> {/* Reduced padding */}
-            {/* Logo */}
+        <Flex as="header" bg="black" p={3} color="white" align="center">
+            {/* Logo and Title */}
             <Link as={RouterLink} to="/">
-                <Box p="1">
-                    <Image src="/image.png" alt="Logo" boxSize="50px" />
-                </Box>
+                <Flex align="center">
+                    <Box p="1">
+                        <Image src="/image.png" alt="Logo" boxSize="50px" />
+                    </Box>
+                    <Text fontSize="lg" fontWeight="bold" ml={2}>IELTSBank</Text>
+                </Flex>
             </Link>
 
             <Spacer />
 
-            {/* Navigation Item */}
-            <Button {...buttonStyle} size="md">
-                Log In
-            </Button>
+            {/* Menu Icon */}
+            <IconButton
+                icon={<HamburgerIcon color="white"/>}
+                variant="outline"
+                aria-label="Menu"
+                onClick={onToggle}
+                colorScheme="whiteAlpha" // This will ensure the icon and border are white
+            />
+
+            {/* Expandable Menu Items */}
+            <Collapse in={isOpen} animateOpacity>
+                <Flex
+                    direction="column"
+                    bg="black"
+                    p={4}
+                    rounded="md"
+                    shadow="md"
+                >
+                    {/* Menu items can be added here */}
+                    <Text color="white">Menu Item 1</Text>
+                    <Text color="white">Menu Item 2</Text>
+                    {/* ... other items */}
+                </Flex>
+            </Collapse>
         </Flex>
     );
 }
