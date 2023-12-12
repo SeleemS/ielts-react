0417a78cbb124367239f6eba38d41f4850e6b42d
Seleem <sseleem1601@gmail.com>
2023-12-12 15:28:15 -0500

    Added boilerplate for listeningquestion

--- src/pages/ListeningQuestion.js
+++ src/pages/ListeningQuestion.js
@@ -21,9 +21,8 @@ const ListeningQuestion = () => {
     const [audioUrl, setAudioUrl] = useState('');
     const [passageTitle, setPassageTitle] = useState('');
     const [userResponse, setUserResponse] = useState('');
-    // ... other state variables
+    // ... other state variables and hooks
 
-    // Use useParams to get the docId from the URL
     const { id: docId } = useParams();
 
     useEffect(() => {
@@ -34,7 +33,7 @@ const ListeningQuestion = () => {
 
             if (docSnap.exists()) {
                 const data = docSnap.data();
-                setAudioUrl(data.audioUrl); // Load the audio URL
+                setAudioUrl(data.audioUrl);
                 setPassageTitle(data.passageTitle);
             } else {
                 console.log("No such document!");
@@ -46,24 +45,82 @@ const ListeningQuestion = () => {
 
     // ... other functions and handlers
 
+    const handleResponseChange = (event) => {
+        setUserResponse(event.target.value);
+    };
+
+    // ... other necessary functions
+
     return (
         <>
             <Helmet>
-                {/* Update Helmet content */}
+                {/* Add Helmet content similar to WritingQuestion */}
             </Helmet>
             <Navbar />
             <Container maxW="container.xl">
-                <Flex direction={{ base: "column", md: "row" }} spacing={8} align="stretch" my={5}>
-                    <Box flex="1" p={5} shadow="md" borderWidth="1px" overflowY="auto" maxH={{ base: "33vh", md: "75vh" }}>
-                        <Text fontSize="large" fontWeight="bold">{passageTitle}:</Text>
-                        <Divider my={4} />
-                        <audio src={audioUrl} controls>
-                            Your browser does not support the audio element.
-                        </audio>
+                <Flex 
+                    direction={{ base: "column", md: "row" }} 
+                    spacing={8} 
+                    align="stretch" 
+                    my={5}
+                >
+                    <Box 
+                        flex="1" 
+                        p={5} 
+                        shadow="md" 
+                        borderWidth="1px" 
+                        overflowY="auto" 
+                        maxH={{ base: "33vh", md: "75vh" }}
+                        minH={{ base: "33vh", md: "75vh" }}
+                        mt={{ base: -2, md: 0 }}
+                        mb={{ base: 3, md: 0 }}
+                        mx={{ md: 2 }}
+                    >
+                        <Text fontSize="large" fontWeight="bold" mb={3}>{passageTitle}:</Text>
+                        <Flex 
+                            direction="column" 
+                            alignItems="center" 
+                            justifyContent="center"
+                            h="100%" // Full height of the container
+                        >
+                            <audio src={audioUrl} controls style={{ width: '100%', maxWidth: '400px' }}>
+                                Your browser does not support the audio element.
+                            </audio>
+                        </Flex>
                     </Box>
-                    {/* User response box and submission button */}
+    
+                    <Box 
+                        flex="1" 
+                        p={5} 
+                        shadow="md" 
+                        borderWidth="1px" 
+                        overflowY="auto" 
+                        display="flex" 
+                        flexDirection="column" 
+                        maxH={{ base: "33vh", md: "75vh" }}
+                        minH={{ base: "33vh", md: "75vh" }}
+                    >
+                        <Flex alignItems="center">
+                            <Text fontSize="lg" fontWeight="bold" mr={2}>Your Response:</Text>
+                            {/* Add word count and other UI elements if needed */}
+                        </Flex>
+                        <Divider my={4} />
+                        <Textarea
+                            placeholder="Type your response here..."
+                            size="lg"
+                            flex="1" 
+                            minHeight="0" 
+                            value={userResponse}
+                          
+                        />
+                    </Box>
                 </Flex>
-                {/* Modals and other components */}
+                <Flex justifyContent="center" mt={-1}>
+                    <Button bg="black" colorScheme="blue" >
+                        Submit
+                    </Button>
+                </Flex>
+                {/* Add Modals for loading, information, and results similar to WritingQuestion */}
             </Container>
         </>
     );
