0d6ad8252b3c3ee54fb0f6ce7e24669544e13181
Seleem <sseleem1601@gmail.com>
2023-12-12 12:59:59 -0500

    Update router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ReadingQuestion from './pages/ReadingQuestion';
 import WritingQuestion from './pages/WritingQuestion';
