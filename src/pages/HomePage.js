f3d494f135630ac49792895e6e55d08470d77555
Seleem <127633573+SeleemS@users.noreply.github.com>
2024-09-20 11:57:18 +0300

    Try animating ad spaces again

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,6 +10,13 @@ import Navbar from '../components/Navbar';
 import Toggle from '../components/Toggle';
 import DataTable from '../components/DataTable';
 import Footer from '../components/Footer';
+import { keyframes} from '@chakra-ui/react';
+
+const moveStripes = keyframes`
+  0% { background-position: 0% 50%; }
+  100% { background-position: 100% 50%; }
+`;
+
 
 const HomePage = () => {
     const adContainerRef = useRef(null);
@@ -38,11 +45,22 @@ const HomePage = () => {
                 justify="space-between"
                 align="center"
                 wrap="wrap"
-                px={{ md: 8 }} // Horizontal padding on medium and larger screens
+                px={{ md: 8 }}
                 py={6}
             >
-                {/* Left Ad Container */}
-                <Flex display={adDisplay} width="300px" height="600px" bg="gray.200" mx={2} justifyContent="center" alignItems="center">
+                {/* Left Ad Container with animated stripes */}
+                <Flex
+                    display={adDisplay}
+                    width="300px"
+                    height="600px"
+                    bg="gray.200"
+                    bgGradient="linear(to-r, gray.300, white, gray.300)"
+                    bgSize="200% 100%"
+                    animation={`${moveStripes} 3s linear infinite`}
+                    mx={2}
+                    justifyContent="center"
+                    alignItems="center"
+                >
                     <ins className="adsbygoogle"
                         style={{ display: "block" }}
                         data-ad-client="ca-pub-5189362957619937"
@@ -51,14 +69,25 @@ const HomePage = () => {
                         data-full-width-responsive="true"></ins>
                 </Flex>
 
-
+                {/* DataTable remains unchanged to ensure full functionality */}
                 <VStack spacing={4} flex="1" minWidth="300px">
                     <Toggle onChange={handleToggleChange} />
                     <DataTable selectedOption={selectedOption} />
                 </VStack>
 
-                {/* Right Ad Container */}
-                <Flex display={adDisplay} width="300px" height="600px" bg="gray.200" mx={2} justifyContent="center" alignItems="center">
+                {/* Right Ad Container with animated stripes */}
+                <Flex
+                    display={adDisplay}
+                    width="300px"
+                    height="600px"
+                    bg="gray.200"
+                    bgGradient="linear(to-r, gray.300, white, gray.300)"
+                    bgSize="200% 100%"
+                    animation={`${moveStripes} 3s linear infinite`}
+                    mx={2}
+                    justifyContent="center"
+                    alignItems="center"
+                >
                     <ins className="adsbygoogle"
                         style={{ display: "block" }}
                         data-ad-client="ca-pub-5189362957619937"
